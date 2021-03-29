ce61543c2a7af07fcd5fed32f3482ab3cc666995
dgraysh <dgraysh@gmail.com>
2021-03-29 16:58:43 +0300

    Initial commit

---
+++ src/main/java/com/dgraysh/IntBot/IntBotApplication.java
@@ -0,0 +1,13 @@
+package com.dgraysh.IntBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntBotApplication.class, args);
+	}
+
+}
